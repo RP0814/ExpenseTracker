8f112f5478f96caf3d128b7726d64f374c8dc4d6
RP0814 <rahulparikh0814@gmail.com>
2026-08-24 18:33:13 +0530

    Add expense API client

---
+++ ExpenseTracker/Networking/ExpenseAPIClient.swift
@@ -0,0 +1,36 @@
+//
+//  ExpenseAPIClient.swift
+//  ExpenseTracker
+//
+//  Created by Apple on 24/08/26.
+//
+
+import Foundation
+
+final class ExpenseAPIClient {
+
+    enum APIError: Error {
+        case invalidResponse
+        case invalidExpenseData
+    }
+
+    func fetchExpenses(
+        from url: URL
+    ) async throws -> [[String: Any]] {
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+              200..<300 ~= httpResponse.statusCode else {
+            throw APIError.invalidResponse
+        }
+
+        let json = try JSONSerialization.jsonObject(with: data)
+
+        guard let expenses = json as? [[String: Any]] else {
+            throw APIError.invalidExpenseData
+        }
+
+        return expenses
+    }
+}
